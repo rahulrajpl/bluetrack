36ba5862f142f439991cbccd5ac917a2c39dcded
Rahul Raj <lukrahul@gmail.com>
2020-02-13 09:50:09 +0530

    removed tutorials and basics folder

---
+++ trial12.py
@@ -0,0 +1,202 @@
+import dash
+from dash.dependencies import Output, Input
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly
+import random
+import plotly.graph_objects as go
+from collections import deque
+import pandas as pd
+import subprocess
+import os
+from time import sleep
+from PIL import Image
+import numpy as np
+
+
+# For reading the last line of sensor data values stored in a file
+def tail():
+    result = subprocess.run(['tail', '-1', 'sensor/GetData/steps.txt'], stdout=subprocess.PIPE)
+    return result.stdout.decode('utf-8') 
+
+# For offline steps plotting
+# file = open('sensor/GetData/steps.txt', 'r') 
+file = open('analytics/steps_train.txt', 'r') 
+# ignoring the first line
+file.readline()
+
+img = Image.open('docs/rm3_1.png')
+
+X = deque(maxlen=15)
+Y = deque(maxlen=15)
+counter = 0
+interval = 1500 # Timer for updating the graph in msec
+
+#Initial data
+data = tail()
+X.append(data.split(',')[1])
+Y.append(data.split(',')[2])
+
+external_stylesheets = [
+    'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    {
+        'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+        'rel': 'stylesheet',
+        'integrity': 'sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
+        'crossorigin': 'anonymous'
+    }
+]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(
+    style={'backgroundColor': '#111111'},
+    children=[
+
+    
+
+    html.Div(
+        className="app-header",
+        children=[
+            html.Div('bluTrack', className="app-header--title"),
+        ]
+    ),
+
+    html.Div(
+        className="app-header",
+        children=[
+            html.P('Pedestrian dead reckoning and Outlier Detection in Trajectory Data', className="app-header--sub-head")
+        ],
+    ),
+    #------------two column divs start------
+    html.Div([
+        html.Div([
+            html.Div(
+            className="app-image",
+            children=[
+                html.Img(src=img, alt='bg_image')
+                ]
+            ),
+
+            html.Div(
+                className="app-plot",
+                id="app-plotid",
+                children=[
+                    dcc.Graph(
+                    id='live-graph',
+                    animate=False,
+                    config={
+                        'autosizable':True,
+                        'scrollZoom':True,
+                        'displayModeBar':'hover',    
+                    }
+                    )
+                ],
+            ),
+
+            dcc.Slider(
+                id='rotate-slider',
+                min=-180,
+                max=180,
+                step=2,
+                value=0
+            ),
+
+            html.Div(id='slider-output-container'),
+
+            dcc.Interval(
+                id='graph-update',
+                interval=interval, 
+                n_intervals=0
+                ),
+        ], className="six columns"),
+
+        html.Div([
+            # html.H3('Column 2'),
+            # dcc.Graph(id='g2', figure={'data': [{'y': [1, 2, 3]}]})
+            html.P('Analytics here. Coming soon', className='header-analytics')
+        ], className="six columns"),
+    ], className="row"),
+    #------two columns divs end------------
+    
+    
+    ]
+)
+
+@app.callback(Output('app-plotid', 'style'), 
+        [Input('rotate-slider', 'value')])
+def update_style(value):
+    return {'transform': 'rotate({}deg)'.format(value)}
+
+@app.callback(
+    dash.dependencies.Output('slider-output-container', 'children'),
+    [dash.dependencies.Input('rotate-slider', 'value')])
+def update_output(value):
+    return 'Drag the slider to set orientation of plot. Current: {} deg'.format(value)
+
+@app.callback(
+    Output('live-graph', 'figure'),
+    [Input('graph-update','n_intervals')]
+)
+def update_graph(n):
+    global X
+    global Y
+    # global counter
+    # counter += interval/1000
+    #-----------------------------------------------------
+    # For real-time plot
+    # new_X = str(tail()).split(',')[1]
+    # new_Y = str(tail()).split(',')[2]
+    #-----------------------------------------------------
+
+    #-----------------------------------------------------
+    # For simulated real-time plot
+    if not file=="":
+        data = file.readline().split(',')
+        new_X, new_Y = data[1], data[2]
+    else:
+        file.seek(0,0)
+    # # print(new_X, new_Y)
+    #-----------------------------------------------------
+
+    if not (X==new_X and Y==new_Y):
+        X.append(new_X)
+        Y.append(new_Y)
+    
+    # # Add trace
+    data = go.Scatter(
+        x = list(X),
+        y = list(Y),
+        name = 'Scatter',
+        mode = 'lines+markers'
+        # mode = 'lines'  
+    )
+
+    x_range = [-10, 65]
+    y_range = [-30, 65]
+
+    layout = go.Layout(xaxis=dict(range=x_range),
+                        yaxis=dict(range=y_range),
+                        height=500,
+                        showlegend=False,
+                        uirevision='graph-update',
+                        paper_bgcolor='rgba(0,0,0,0)',
+                        plot_bgcolor='rgba(0,0,0,0)',
+                        )
+    
+    # Create figure
+    fig = go.Figure(
+        data = [data],
+        layout=layout
+    )
+
+    # Fine tune layout
+    fig.update_layout(template="plotly_white")
+    fig.update_xaxes(showticklabels=False, zeroline=False)
+    fig.update_yaxes(showticklabels=False, zeroline=False)
+    fig.update_layout(xaxis_showgrid=False, yaxis_showgrid=False)
+    return fig
+
+if __name__=='__main__':
+    # app.run_server(debug=True, dev_tools_hot_reload=False)
+    app.run_server(debug=True)
